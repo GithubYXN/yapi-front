1d98b7dcfeeecbc3f83c057fadfa86d4d732f890
GithubYXN <2502869657@qq.com>
2024-08-15 20:13:33 +0800

    Delete redundant code

--- src/pages/Admin/InterfaceInfo/index.tsx
+++ src/pages/Admin/InterfaceInfo/index.tsx
@@ -356,24 +356,7 @@ const TableList: React.FC = () => {
         }}
       />
       {selectedRowsState?.length > 0 && (
-        <FooterToolbar
-          extra={
-            <div>
-              已选择{' '}
-              <a
-                style={{
-                  fontWeight: 600,
-                }}
-              >
-                {selectedRowsState.length}
-              </a>{' '}
-              项 &nbsp;&nbsp;
-              <span>
-                服务调用次数总计 {selectedRowsState.reduce((pre, item) => pre + item.callNo!, 0)} 万
-              </span>
-            </div>
-          }
-        >
+        <FooterToolbar>
           <Button
             onClick={async () => {
               await handleRemove(selectedRowsState);
